726ab246e14473a7d56b5620611d44c73a6151bd
sonia <atresonia@gmail.com>
2021-05-14 11:59:15 -0700

    added instructions, replaced tower image, and increased height between barriers

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -1,12 +1,12 @@
 class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, velocityX, velocityY) {
         // call Phaser Physics Sprite constructor
-        super(scene, width/3, height/2, 'character'); 
+        super(scene, width/3, height - tileSize * 3, 'character'); 
         // set up physics sprite
         scene.add.existing(this);               // add to existing scene, displayList, updateList
         scene.physics.add.existing(this);       // add to physics system
         this.setCollideWorldBounds(true); 
-        this.setScale(0.15);
+        this.setScale(0.1);
         this.setMaxVelocity(velocityX, velocityY);
         this.body.allowGravity=true;
         this.setPushable(false);
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,7 +12,7 @@ class Play extends Phaser.Scene {
         this.load.image('door', 'door.jpeg');
         this.load.image('background', 'background.jpeg');
         this.load.image('character', 'character.png');
-        this.load.image('tower', 'tower.jpeg');
+        this.load.image('tower', 'tower.png');
     }
 
     create() {
@@ -30,6 +30,7 @@ class Play extends Phaser.Scene {
         cursors = this.input.keyboard.createCursorKeys();
 
         this.addBackground();
+        this.addInstructions();
         this.makePlatforms();
         this.addCharacter();
         this.addColliders();
@@ -39,47 +40,46 @@ class Play extends Phaser.Scene {
         this.add.image(width, height, 'background');
     }
 
+    addInstructions() {
+        this.add.text(100, 100, 'Instructions: use right and left arrow keys to' +
+            ' move and the up arrow key to jump. Use the spacebar to build ' + 
+            'towers to reach greater heights', {fontFamily: 'Courier', fontSize: '20px', 
+            color: '#fff', lineSpacing: 10, wordWrap: { width: width/3,},
+        });
+    }
+
     makePlatforms() {
         //add platform tiles
         this.platforms = this.add.group();
         //ground
         for (let i = 0; i < width; i += tileSize) {
-            let groundTile = this.physics.add.sprite(i , height - tileSize * 1.5, 'tile')
-            .setScale(SCALE, SCALE/4).setOrigin(0);
+            let groundTile = this.physics.add.sprite(i, height - tileSize * 1.5, 'tile')
+                .setScale(SCALE, SCALE / 4).setOrigin(0);
             groundTile.body.allowGravity = false;
             groundTile.body.setImmovable();
             this.platforms.add(groundTile);
         }
 
         //platform 1
-        for(let i = tileSize*13; i < width-tileSize*8; i += tileSize) {
-            let groundTile = this.physics.add.sprite(i, height - tileSize*5, 'tile')
-            .setScale(SCALE, SCALE/5).setOrigin(0);
+        for (let i = tileSize * 13; i < width - tileSize * 8; i += tileSize) {
+            let groundTile = this.physics.add.sprite(i, height - tileSize * 5, 'tile')
+                .setScale(SCALE, SCALE / 5).setOrigin(0);
             groundTile.body.immovable = true;
             groundTile.body.allowGravity = false;
             this.platforms.add(groundTile);
         }
 
-        // //platform 2
-        // for(let i = tileSize*2; i < width-tileSize*20; i += tileSize) {
-        //     let groundTile = this.physics.add.sprite(i, height - tileSize*9, 'tile')
-        //     .setScale(SCALE, SCALE/5).setOrigin(0);
-        //     groundTile.body.immovable = true;
-        //     groundTile.body.allowGravity = false;
-        //     this.platforms.add(groundTile);
-        // }
-
         //platform 3
-        for(let i = tileSize*10; i < width-tileSize*13; i += tileSize) {
-            let groundTile = this.physics.add.sprite(i, height - tileSize*13, 'tile')
-            .setScale(SCALE, SCALE/5).setOrigin(0);
+        for (let i = tileSize * 10; i < width - tileSize * 13; i += tileSize) {
+            let groundTile = this.physics.add.sprite(i, height - tileSize * 20, 'tile')
+                .setScale(SCALE, SCALE / 5).setOrigin(0);
             groundTile.body.immovable = true;
             groundTile.body.allowGravity = false;
             this.platforms.add(groundTile);
         }
 
         //door at top to next level
-        let door = this.add.image(width/2 + 30, height - tileSize*13.9, 'door');
+        let door = this.add.image(width / 2 + 30, height - tileSize * 20.9, 'door');
         door.setScale(0.1);
     }
 
@@ -92,54 +92,58 @@ class Play extends Phaser.Scene {
     }
 
     update() {
+        this.keyDetection();
+        this.jumpingLogic();
+    }
+
+    //logic for keys pressed
+    keyDetection() {
         //left arrow key down
         if (cursors.left.isDown) {
             this.player.body.setAccelerationX(-this.ACCELERATION);
-        } else if (cursors.right.isDown) {
+        } else if (cursors.right.isDown) {  //right arrow key down
             this.player.body.setAccelerationX(this.ACCELERATION);
-        } 
-        else if(this.spacebar.isDown){
-            this.buildTower(this.player);
-        }
-        else {
+        } else if (this.spacebar.isDown) {    //spacebar key down
+            this.buildTower();
+        } else {
             //set acceleration to 0 so drag will take over
             this.player.body.setAccelerationX(0);
             this.player.body.setDragX(this.DRAG);
         }
-        this.jumpingLogic();
     }
 
     //jump logic for player
     jumpingLogic() {
         this.player.isGrounded = this.player.body.touching.down;
-        if(this.player.isGrounded) {
-	    	this.jumps = this.MAX_JUMPS;
-	    	this.jumping = false;
-	    } else {
+        if (this.player.isGrounded) {
+            this.jumps = this.MAX_JUMPS;
+            this.jumping = false;
+        } else {
             console.log("not grounded");
         }
 
         // allow steady velocity change up to a certain key down duration
         // see: https://photonstorm.github.io/phaser3-docs/Phaser.Input.Keyboard.html#.DownDuration__anchor
-	    if(this.jumps > 0 && Phaser.Input.Keyboard.DownDuration(cursors.up, 250)) {
+        if (this.jumps > 0 && Phaser.Input.Keyboard.DownDuration(cursors.up, 250)) {
             console.log("jump");
-	        this.player.body.velocity.y = this.JUMP_VELOCITY;
-	        this.jumping = true;
-	    } else {
-	    	console.log("not pressing up key");
-	    }
+            this.player.body.velocity.y = this.JUMP_VELOCITY;
+            this.jumping = true;
+        } else {
+            console.log("not pressing up key");
+        }
         // finally, letting go of the UP key subtracts a jump
         // see: https://photonstorm.github.io/phaser3-docs/Phaser.Input.Keyboard.html#.UpDuration__anchor
-	    if(this.jumping && Phaser.Input.Keyboard.UpDuration(cursors.up)) {
-	    	this.jumps--;
-	    	this.jumping = false;
-	    }
+        if (this.jumping && Phaser.Input.Keyboard.UpDuration(cursors.up)) {
+            this.jumps--;
+            this.jumping = false;
+        }
     }
-    buildTower(player){
-        this.tower=this.physics.add.sprite(player.x+80,player.y+40,'tower');
+    buildTower() {
+        this.tower = this.physics.add.sprite(this.player.x + 80, this.player.y - 30, 'tower');
+        this.tower.setScale(0.2);
         this.tower.body.immovable = true;
         this.tower.body.allowGravity = false;
-        this.physics.add.collider(this.tower,player);
-        this.physics.add.collider(this.platforms,this.tower);
+        this.physics.add.collider(this.tower, this.player);
+        this.physics.add.collider(this.platforms, this.tower);
     }
 }
