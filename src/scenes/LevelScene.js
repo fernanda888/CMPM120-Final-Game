50872fee6b521502f42287dd2b05a3c7bdb80dfc
Brian Zhang <bzhang72@ucsc.edu>
2021-05-13 17:55:30 -0700

    Fixed level button placement

--- src/scenes/LevelScene.js
+++ src/scenes/LevelScene.js
@@ -16,11 +16,11 @@ class LevelScene extends Phaser.Scene {
         this.add.image(game.config.width, game.config.height, 'levelTower').setOrigin(1,1);
 
         //adds three level selections to the menu
-        var playButton1 = this.add.image(game.config.width/2 + 7.5,
+        var playButton3 = this.add.image(game.config.width/2 + 7.5,
             game.config.height/3 + 36.5, 'button3');
         var playButton2 = this.add.image(game.config.width/2 + 7.5,
             game.config.height/2 + 132, 'button2');
-        var playButton3 = this.add.image(game.config.width/2 + 7.5,
+        var playButton1 = this.add.image(game.config.width/2 + 7.5,
             game.config.height/1.5 + 236.5, 'button1');
 
         //foreground rocks
