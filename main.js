99b0f13b80f8d9cd9c19a045bbde9939cd3e9316
fernanda888 <60996563+fernanda888@users.noreply.github.com>
2021-05-25 17:07:28 -0700

    added play

---
+++ main.js
@@ -0,0 +1,63 @@
+//global 
+let cursors;
+const SCALE = .5;
+const tileSize = 30;
+
+
+//main game object
+let config = {
+    type: Phaser.CANVAS,
+    /*
+    //prevents edge bleedings
+    render: {
+        pixelArt: true
+    },
+    */
+    width: 1500,
+    height: 1000,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    zoom: 1,
+    scene: [Play, Puzzle1,EndScene]
+  }
+  //define game
+let game = new Phaser.Game(config);
+
+//reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+// set UI sizes
+let borderUISize = game.config.height / 15;
+console.log(borderUISize);
+let scrollSpeed = 4;
+let borderPadding = borderUISize / 3;
+let width = game.config.width;
+let height = game.config.height;
+let centerX = game.config.width/2;
+let centerY = game.config.height/2;
+const textSpacer = 64;
+let score;
+let highScore;
+let cont=false;
+let quit=false
+let playNow=false;
+let newHighScore = false;
+let towerExists=false;
+let playerWalking=false;
+let moveCam=false;
+let facingRight=true;
+let puzzle1Scene=false;
+let foundKey1=false;
+let foundKey2=false;
+let foundKey3=false;
